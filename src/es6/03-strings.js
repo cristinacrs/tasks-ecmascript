3a7cb168e2f5e411c116b8be6f3f80808b1a638e
Cristina Cruz <cristinacruzcortes@gmail.com>
2022-09-08 14:04:42 -0500

    let, const, arrow functions y strings

---
+++ src/es6/03-strings.js
@@ -0,0 +1,18 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//Multi-line strings
+
+let lorem = 'esto es un string\n' + 'esto es otra linea';
+
+let lorem2 = `Esta es la frase 1
+continuacion de la frase`;
+
+console.log(lorem);
+console.log(lorem2);

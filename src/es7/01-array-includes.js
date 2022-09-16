c9972746b76b5771a989ab58ab1bcba2ecfa7a16
Cristina Cruz Cortés <cristinacruzcortes@gmail.com>
2022-09-15 19:27:46 -0500

    features ecmascript7

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,8 @@
+let numbers = [1,2,5,8,24]
+
+console.log(numbers.includes(24));
+console.log(numbers.includes(4));
+
+const names = ['cris', 'lana', 'freya', 'keisi', 'pelusa'];
+
+console.log(names.includes('Lana'));

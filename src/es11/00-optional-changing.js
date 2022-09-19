2defa5ee0d8bdf9cdcfa3c98458649e2f33f818e
Cristina Cruz Cortés <cristinacruzcortes@gmail.com>
2022-09-19 17:11:41 -0500

    features ecmascript11

---
+++ src/es11/00-optional-changing.js
@@ -0,0 +1,11 @@
+const users = {
+    cris : {
+        country: 'MX'
+    },
+    jorge : {
+        country: 'CA'
+    },
+}
+
+console.log(users.cris.country);
+console.log(users?.freya?.country);
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+const  aBigNumber = 12345678544548655656n;
+const anotherBigNumber = BigInt(12345678544548655656);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
